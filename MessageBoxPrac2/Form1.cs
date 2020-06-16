3dfa867dc209a869206da6af7b6c2ed6f1c46b95
Gihun Ham <progh2@gmail.com>
2020-06-16 10:41:10 +0900

    초기 커밋.

---
+++ MessageBoxPrac2/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MessageBoxPrac2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("내용 블라블라");
+            MessageBox.Show("내용 블라블라", "제목~");
+            MessageBox.Show("내용 블라블라", "제목~", MessageBoxButtons.RetryCancel);
+            MessageBox.Show("내용 블라블라", "제목~", 
+                MessageBoxButtons.AbortRetryIgnore,
+                MessageBoxIcon.Asterisk);
+        }
+    }
+}
